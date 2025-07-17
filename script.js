db39aa4d3fb5fffeb634150e37ccedfd84fd2697
Jesus Pedro Bapolo <jesusestudante@gmail.com>
2025-07-17 11:43:48 +0100

    update

--- script.js
+++ script.js
@@ -5,94 +5,102 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnCopy = document.querySelector("#btnCopy")
     const toast = document.querySelector("#toast")
     const select = document.querySelector("select")
-  
+
     const arrayInput = [...inputs]
-  
+
     arrayInput.forEach(input => {
-      input.addEventListener("input", (event) => {
-  
-        const valorDosInputs = arrayInput.map(input => input.value || 0)
-  
-        if (parseInt(select.value) !== 8) {
-  
-          box.style.borderRadius = `${valorDosInputs[0]}px ${valorDosInputs[1]}px ${valorDosInputs[2]}px ${valorDosInputs[3]}px`
-  
-        } else {
-  
-          box.style.borderRadius = `${valorDosInputs[0]}px ${valorDosInputs[1]}px ${valorDosInputs[2]}px ${valorDosInputs[3]}px / ${valorDosInputs[4]}px ${valorDosInputs[5]}px ${valorDosInputs[6]}px ${valorDosInputs[7]}px`
-  
-        }
-      })
+        input.addEventListener("input", (event) => {
+
+            const valorDosInputs = arrayInput.map(input => input.value || 0)
+
+            if (parseInt(select.value) !== 8) {
+
+                box.style.borderRadius = `${valorDosInputs[0]}px ${valorDosInputs[1]}px ${valorDosInputs[2]}px ${valorDosInputs[3]}px`
+
+            } else {
+
+                box.style.borderRadius = `${valorDosInputs[0]}px ${valorDosInputs[1]}px ${valorDosInputs[2]}px ${valorDosInputs[3]}px / ${valorDosInputs[4]}px ${valorDosInputs[5]}px ${valorDosInputs[6]}px ${valorDosInputs[7]}px`
+
+            }
+        })
     })
-  
+
     button.addEventListener("click", () => {
-      arrayInput.forEach(input => {
-        const valorDosInputs = arrayInput.map(input => input.value || 0)
-  
-        if (arrayInput.length !== 8) {
-  
-          box.style.borderRadius = `${valorDosInputs[0]}px ${valorDosInputs[1]}px ${valorDosInputs[2]}px ${valorDosInputs[3]}px`
-  
-        } else {
-  
-          box.style.borderRadius = `${valorDosInputs[0]}px ${valorDosInputs[1]}px ${valorDosInputs[2]}px ${valorDosInputs[3]}px / ${valorDosInputs[4]}px ${valorDosInputs[5]}px ${valorDosInputs[6]}px ${valorDosInputs[7]}px`
-  
-        }
-      })
+        arrayInput.forEach(input => {
+            const valorDosInputs = arrayInput.map(input => input.value || 0)
+
+            if (arrayInput.length !== 8) {
+
+                box.style.borderRadius = `${valorDosInputs[0]}px ${valorDosInputs[1]}px ${valorDosInputs[2]}px ${valorDosInputs[3]}px`
+
+            } else {
+
+                box.style.borderRadius = `${valorDosInputs[0]}px ${valorDosInputs[1]}px ${valorDosInputs[2]}px ${valorDosInputs[3]}px / ${valorDosInputs[4]}px ${valorDosInputs[5]}px ${valorDosInputs[6]}px ${valorDosInputs[7]}px`
+
+            }
+        })
     })
-  
+
     btnCopy.addEventListener("click", () => {
-      let copyText
-  
-      if (parseInt(select.value) !== 8) {
-        const [input1, input2, input3, input4] = inputs
-  
-        input1.value = input1.value || 0;
-        input2.value = input2.value || 0;
-        input3.value = input3.value || 0;
-        input4.value = input4.value || 0;
-  
-        copyText = `border-radius: ${input1.value}px ${input2.value}px ${input3.value}px ${input4.value}px ;`
-      } else {
-        const [input1, input2, input3, input4, input5, input6, input7, input8] = inputs
-  
-        input1.value = input1.value || 0;
-        input2.value = input2.value || 0;
-        input3.value = input3.value || 0;
-        input4.value = input4.value || 0;
-        input5.value = input5.value || 0;
-        input6.value = input6.value || 0;
-        input7.value = input7.value || 0;
-        input8.value = input8.value || 0;
-  
-        copyText = `border-radius: ${input1.value}px ${input2.value}px ${input3.value}px ${input4.value}px / ${input5.value}px ${input6.value}px ${input7.value}px ${input8.value}px ;`
-      }
-  
-        toast.classList.add('active')
-      setTimeout(() => {
-        toast.classList.remove('active')
-      }, 3000)
-  
-      navigator.clipboard.writeText(copyText)
+        let copyText
+
+        if (parseInt(select.value) !== 8) {
+            const [input1, input2, input3, input4] = inputs
+
+            input1.value = input1.value || 0;
+            input2.value = input2.value || 0;
+            input3.value = input3.value || 0;
+            input4.value = input4.value || 0;
+
+            copyText = `border-radius: ${input1.value}px ${input2.value}px ${input3.value}px ${input4.value}px ;`
+        } else {
+            const [input1, input2, input3, input4, input5, input6, input7, input8] = inputs
+
+            input1.value = input1.value || 0;
+            input2.value = input2.value || 0;
+            input3.value = input3.value || 0;
+            input4.value = input4.value || 0;
+            input5.value = input5.value || 0;
+            input6.value = input6.value || 0;
+            input7.value = input7.value || 0;
+            input8.value = input8.value || 0;
+
+            copyText = `border-radius: ${input1.value}px ${input2.value}px ${input3.value}px ${input4.value}px / ${input5.value}px ${input6.value}px ${input7.value}px ${input8.value}px ;`
+        }
+
+        mostrarToast(toast)
+
+        copyText(copyText)
+    })
+
+    select.addEventListener("change", () => {
+        if (parseInt(select.value) !== 8) {
+            visibilidadeDosUltimoInputs(arrayInput, "block")
+        } else {
+            visibilidadeDosUltimoInputs(arrayInput, "none")
+        }
+    })
+})
+
+function visibilidadeDosUltimoInputs(array, visibilidade) {
+    for (let contador = 4; contador <= array.length - 1; contador++) {
+        array[contador].style.display = visibilidade
+    }
+}
+
+function copyText(text) {
+    navigator.clipboard.writeText(text)
         .then(() => {
-          console.log("Copiado:", copyText);
+            console.log("Copiado:", text);
         })
         .catch(err => {
-          console.error("Erro ao copiar:", err);
+            console.error("Erro ao copiar:", err);
         });
-    })
-  
-    select.addEventListener("change", () => {
-      if (parseInt(select.value) !== 8) {
-        visibilidadeDosUltimoInputs(arrayInput, "block")
-      } else {
-        visibilidadeDosUltimoInputs(arrayInput, "none")
-      }
-    })
-  })
-  
-  function visibilidadeDosUltimoInputs(array, visibilidade) {
-    for (let contador = 4; contador <= array.length - 1; contador++) {
-      array[contador].style.display = visibilidade
-    }
-  }
+}
+
+function mostrarToast(toast) {
+    toast.classList.add('active')
+    setTimeout(() => {
+        toast.classList.remove('active')
+    }, 3000)
+}
