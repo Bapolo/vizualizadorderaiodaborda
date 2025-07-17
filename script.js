d25a8cf60f68840e92c1670b2879be184437c5c2
Jesus Pedro Bapolo <jesusestudante@gmail.com>
2025-07-17 11:56:36 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const box = document.querySelector(".box")
-    const inputs = document.querySelectorAll(".inputs input")
+    const inputs = document.querySelectorAll(".values")
     const btnCopy = document.querySelector("#btnCopy")
     const toast = document.querySelector("#toast")
     const select = document.querySelector("select")
